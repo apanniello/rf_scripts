7bfda8d2eb08127d85a7dc2a1234313bbba80a8e
Attilio Panniello <apanniello@logitech.com>
2022-05-19 19:21:54 +0200

    PER : Sensitivity extracted (no att cal)

--- test/PER_process.py
+++ test/PER_process.py
@@ -24,6 +24,7 @@ import pandas as pd
 
 calibration_meas = './Savituck_TRM-2Mbps-01.csv'
 per_meas = './Savituck_RCV-2Mbps-01.csv'
+attenuator_calibration_file = './J7211A_Correction.csv'
 
 minChanel = 3
 maxChanel = 79
@@ -31,6 +32,8 @@ maxChanel = 79
 calibration_chanels = maxChanel- minChanel +1
 calibration_fields = ['Channel', 'H1 (dBm)']
 calibration_df = pd.read_csv(calibration_meas, skiprows=1,nrows=calibration_chanels, usecols=calibration_fields, sep=',', na_values=" , ")
+attenuator_cal_fields = ['Attenuation Setting', '2450']
+attenuator_cal_df = pd.read_csv(attenuator_calibration_file, skiprows=1, usecols=attenuator_cal_fields, sep=',', na_values=" , ")
 
 trmCalibrationPwr = 8
 targetPERrd = 0.14
@@ -47,6 +50,13 @@ attenuationSteps = len(attenuationValues) # number of different attenuations use
 PER_lines = calibration_chanels*attenuationSteps
 PER_fields = ['PerRx(%)', 'Channel','Attenuation']
 PER_df = pd.read_csv(per_meas, skiprows=1,nrows=PER_lines, usecols=PER_fields, converters={'PerRx(%)':p2f}, sep=',', na_values=" , ")
+
+##########################################################################
+#
+# Debug printings
+#
+print(f'calibration_df start : {calibration_df.head()}')
+print(f'--------------------------------------------')
 print(f'PER_df start : {PER_df.head()}')
 print(f'--------------------------------------------')
 sorted_PER_df = PER_df[PER_df['PerRx(%)'] >= targetPERrd]
@@ -75,24 +85,49 @@ print(f'--------------------------------------------')
 #ANSWER
 # df[(df['Points']==0) & (df['Day']==df[df['Points']==0]['Day'].min())]
 
-rows = []
+RxPwr_column = []
 sensitivity_PER_df = pd.DataFrame()
+# channel_df_fields = ['PerRx(%)','Channel','Attenuation','Rx InputPower [dBm]']
+# channel_df_min = pd.DataFrame(columns=channel_df_fields)
+
 for i in range(minChanel, maxChanel+1, 1):
     channel_df = sorted_PER_df[sorted_PER_df['Channel']==i]
     channel_min = sorted_PER_df[sorted_PER_df['Channel']==i]['PerRx(%)'].min()
     channel_df_min = channel_df[channel_df['PerRx(%)'] == channel_min]
+    # channel_df_min['Rx InputPower [dBm]'] = ""
+    # channel_df_min.loc[:,'Rx InputPower [dBm]'] = i
+    # channel_df_min['Rx InputPower [dBm]'] = i
+    attenuation_mask = channel_df_min['Channel']==i
+    calibration_mask = calibration_df['Channel'] == i
+    # Attenuation = channel_df_min[channel_df_min['Channel']==i]['Attenuation'].item()
+    # DF.loc[DF.someCondition=condition, 'A'].item() : to avoid chained-indexing
+    # Attenuation = channel_df_min.loc[channel_df_min['Channel']==i,'Attenuation'].item()
+    attenuation = channel_df_min.loc[attenuation_mask,'Attenuation'].item()
+    # DF.loc[DF.someCondition=condition, 'A'].item()
+    # RxPwr = calibration_df[calibration_mask]['H1 (dBm)'] - attenuation : chained-indexing
+    RxPwr = calibration_df.loc[calibration_mask,'H1 (dBm)'].item() - attenuation # : to avoid chained-indexing
+    RxPwr_column.append(RxPwr)
     sensitivity_PER_df=sensitivity_PER_df.append(channel_df_min, ignore_index=True)
 
 # sensitivity_PER_df = sensitivity_PER_df.reset_index()
+sensitivity_PER_df['Sensitivity [dBm]'] = RxPwr_column
 
-print(f'channel_df : {channel_df}')
+print(f'channel_df :\n {channel_df}')
+print(f'type channel_df : {type(channel_df)}')
 print(f'--------------------------------------------')
 print(f'channel_min : {channel_min}')
 print(f'--------------------------------------------')
 print(f'channel_df_@min : {channel_df_min}')
 print(f'--------------------------------------------')
 print(f'--------------------------------------------')
-print(f'sensitivity_PER_df : {sensitivity_PER_df}')
+print(f'sensitivity_PER_df :\n {sensitivity_PER_df}')
+print(f'--------------------------------------------')
+print(f'--------------------------------------------')
+print(f'attenuator_cal_df : {attenuator_cal_df}')
+print(f'--------------------------------------------')
+print(f'--------------------------------------------')
+print(f'last attenuation : {attenuation}')
+print(f'last RxPwr : {RxPwr}')
 
 
 ##########################################################################
@@ -103,4 +138,5 @@ if sensitivity_PER_df is not None:
     # pd.concat([df3, df4], axis=1)).to_csv('foo.csv')
     sensitivity_PER_df.to_csv(csv_path, header=True, index=False)   # write file to drive
     print(f'File {csv_path} saved.')
-
+#
+##########################################################################
